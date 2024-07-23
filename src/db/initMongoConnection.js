f1d44e6b883888a171ba55e3b4e574a41beff6d0
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-07-23 12:43:58 +0300

    add

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import Contact from '../models/contact';
+import Contact from "../models/contact.js";
 
 dotenv.config();
 
 async function initMongoConnection() {
   const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } = process.env;
 
-  const DB_URI = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`;
+  const DB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
 
   try {
     await mongoose.connect(DB_URI, {
--- src/index.js
+++ src/index.js
@@ -5,15 +5,9 @@ import './models/contact.js';
 
 const app = async () => {
   try {
-    console.log('DB_URI:', process.env.DB_URI);
-
-    if (!process.env.DB_URI) {
-      throw new Error('DB_URI is not defined');
-    }
-
-    await initMongoConnection();
-
-    setupServer();
+ await initMongoConnection();
+  
+ setupServer();
   } catch (error) {
     console.error('Error starting the application:', error);
   }
