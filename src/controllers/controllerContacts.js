1b0d7a8092e6fe6312a26066746c81720ca5c693
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-08-02 11:06:06 +0300

    add

--- src/controllers/controllerContacts.js
+++ src/controllers/controllerContacts.js
@@ -4,13 +4,13 @@ async function getContacts(req, res) {
   try {
     const contacts = await getAllContacts();
     res.status(200).json({
-      status: 'success',
+      status: '200',
       message: 'Successfully found contacts!',
       data: contacts,
     });
   } catch (error) {
     res.status(500).json({
-      status: 'error',
+      status: '500',
       message: 'Failed to retrieve contacts!',
       data: error.message,
     });
