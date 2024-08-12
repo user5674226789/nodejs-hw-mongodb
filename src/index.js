4ef96babc8daa28ce4be7c891899340f267f3aa4
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-08-12 17:52:42 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-import 'dotenv/config';
-import setupServer from './server.js';
-import { initMongoConnection } from './db/initMongoConnection.js';
-import './models/contact.js';
-
-const app = async () => {
-  try {
- await initMongoConnection();
-  
- setupServer();
-  } catch (error) {
-    console.error('Error starting the application:', error);
-  }
+export const SORT_ORDER = {
+  ASC: 'asc',
+  DESC: 'desc',
 };
-
-app();
--- src/validation/contact.js
+++ src/validation/contact.js
@@ -2,6 +2,26 @@ import Joi, { string } from "joi";
 
 export const createContactSchema = Joi.object ({
     name: Joi.string().min(3).max(20).required().messages({
-        'string.base': 
-    })
-})
+        'string.base': 'Username should be a string',
+        'string.min': 'Username should have at least {#limit} characters',
+        'string.max': 'Username should have at most {#limit} characters',
+        'any.required': 'Username is required',
+    }),
+    phoneNumber: Joi.string().min(3).max(20).required().messages({
+        'string.base': 'PhoneNumber should be a string',
+        'string.min': 'PhoneNumber should have at least {#limit} characters',
+        'string.max': 'PhoneNumbere should have at most {#limit} characters',
+        'any.required': 'PhoneNumber is required',
+    }),
+    email: Joi.string().email(),
+    contactType: Joi.string().valid('work', 'home', 'personal').required(),
+    isFavourite: Joi.boolean(),
+  });
+  
+  export const updateContactSchema = Joi.object({
+    name: Joi.string().min(3).max(20),
+    phoneNumber: Joi.string().min(3).max(20),
+    email: Joi.string().email(),
+    contactType: Joi.string().valid('work', 'home', 'personal'),
+    isFavourite: Joi.boolean(),
+  });
