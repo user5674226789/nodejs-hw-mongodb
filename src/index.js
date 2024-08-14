305750fed0561786cd3a0d87c9d3f4e9d6f89cdb
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-08-14 19:11:36 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-export const SORT_ORDER = {
-  ASC: 'asc',
-  DESC: 'desc',
+import { initMongoConnection } from './db/initMongoConnection.js';
+import { setupServer } from './server.js';
+
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
 };
+
+bootstrap();
