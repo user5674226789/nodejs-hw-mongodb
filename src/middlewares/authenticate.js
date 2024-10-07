c34558a3e5c82a2ad9622ee66b1e674d80fe84a8
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-10-07 16:24:36 +0300

    Update authenticate.js

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,7 +1,7 @@
 import createHttpError from 'http-errors';
 
 import { SessionsCollection } from '../db/models/Session.js';
-import { UsersCollection } from '../db/models/user.js';
+import { UsersCollection } from '../db/models/User.js';
 
 export const authenticate = async (req, res, next) => {
   const authHeader = req.get('Authorization');
